394c8d072b9efa6750e9cc75c9650cf0e3c014cd
JW <digitalhumanlife@gmail.com>
2023-04-09 16:08:48 +0900

    0409 home

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -1,8 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+error NftMarketplace__PriceMustAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+
 contract NftMarketplace {
-    function listItem(address nftAddress, uint256 tokenId, uint256 price) external {}
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 tokenId,
+        uint256 price
+    );
+
+    // NFT contract address -> token ID -> listing?
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    ////////////////////
+    // Modifiers      //
+    ////////////////////
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    ////////////////////
+    // Main functions //
+    ////////////////////
+
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    ) external notListed(nftAddress, tokenId, msg.sender) {
+        if (price <= 0) {
+            revert NftMarketplace__PriceMustAboveZero();
+        }
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketplace__NotApprovedForMarketplace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
 }
 
 // 1. `listItem`: list NFTs on the marketplace
